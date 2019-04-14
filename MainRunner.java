fe7e43cec0cff1cf9f5c6f3e2fad6a63fc1244eb
briceybrit <pbrice@seas.upenn.edu>
2019-04-14 12:47:39 -0500

    Committing MainRunner class

---
+++ MainRunner.java
@@ -0,0 +1,34 @@
+
+
+/**
+ * This is the Homework for Recitation Week 11
+ * @author briceybrit
+ *
+ */
+public class MainRunner {
+	
+	/**
+	 * This is the main method which will run this code.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// This is the runner method.
+	}
+}
+
+/**
+ * Below is what I did for/with Keith
+ */
+//With Keith:
+//1)	Peter initiated empty git folder
+//2)	Pulled Keith’s git repository from GitHub
+//3)	Peter made a change to a file
+//4)	Pushed it to Keith’s repository on GitHub. Worked, check.
+//5)	I made a change to a text file
+//6)	Keith made a change to the same file
+//7)	I pushed it online
+//8)	Keith could then not push his, so he pulled it? Remerged it?
+//9)	Keith could view both his and my changes, and rectified the changes.
+//10)	Then he pushed the new version online
+//11)	Peter downloaded the rectified file.
+
--- README.md
+++
@@ -1,4 +0,0 @@
-# UPennMCIT_591
-This is my repository for files related to Java Programming Class 591
-
-Added code
